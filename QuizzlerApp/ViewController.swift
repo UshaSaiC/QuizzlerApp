37ff7e2c416d22dd9ec67bed378b3ee89c1c5fbc
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-05 01:15:36 +0530

    Ensured that app displays questions continuosuly without crashes

--- QuizzlerApp/ViewController.swift
+++ QuizzlerApp/ViewController.swift
@@ -14,9 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
-    let quizQuestions = [
-    "4+2 = 6", "8-3 = 5", "6+3 = 8"
-    ]
+    // data type of quizQuestions is 2D array of string i.e [[String]]
+    let quizQuestions = [["4+2 = 6", "True"], ["8-3 = 5", "True"], ["6+3 = 8", "False"]]
     
     var questionNumber = 0
     
@@ -26,12 +25,27 @@ class ViewController: UIViewController {
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        questionNumber += 1
+        let answerChoosen = sender.currentTitle!
+        let actualAnswer = quizQuestions[questionNumber][1]
+        if answerChoosen == actualAnswer{
+            print("Right:)")
+        } else{
+            print("Wrong:(")
+        }
+        
+        print(questionNumber)
+        if questionNumber<quizQuestions.count-1 {
+           questionNumber += 1
+        }else{
+            questionNumber=0
+        }
+        
         updateQuestionsOnUI()
+
     }
     
     func updateQuestionsOnUI(){
-        questionLabel.text = quizQuestions[questionNumber]
+        questionLabel.text = quizQuestions[questionNumber][0]
     }
 }
 
