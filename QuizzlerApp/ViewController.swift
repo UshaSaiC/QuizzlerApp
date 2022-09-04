5f32dd329f493bf0bfe861d93db5d11904e54924
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-05 00:51:47 +0530

    Added 3 questions to app

--- QuizzlerApp/ViewController.swift
+++ QuizzlerApp/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quizQuestions = [
+    "4+2 = 6", "8-3 = 5", "6+3 = 8"
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateQuestionsOnUI()
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        questionNumber += 1
+        updateQuestionsOnUI()
+    }
+    
+    func updateQuestionsOnUI(){
+        questionLabel.text = quizQuestions[questionNumber]
+    }
 }
 
